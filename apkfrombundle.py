4f83bb284dae5919398bd30196d920e81a28dcea
David <137324959+davidgreckosij118@users.noreply.github.com>
2023-08-06 16:45:58 +0300

    Update apkfrombundle.py

--- apkfrombundle.py
+++ apkfrombundle.py
@@ -46,3 +46,5 @@ if (fileName.startswith("BUNDLE-METADATA")):
  if fileName == apkInfo.filename:
  found = True
                 firstFile = apkZip.open(apkInfo, 'r')
+ secondFile = bundleZip.open(bundleInfo, 'r')
+                if compareFiles(firstFile, secondFile) != True:

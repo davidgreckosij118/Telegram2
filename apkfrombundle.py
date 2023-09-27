313f5c12f4c6752076468394a095e076dd08797b
David <137324959+davidgreckosij118@users.noreply.github.com>
2023-09-27 15:08:01 +0300

    Update apkfrombundle.py

--- apkfrombundle.py
+++ apkfrombundle.py
@@ -43,3 +43,5 @@ def compareApkFromBundle(bundle, apk):
             fileName = remove_prefix(fileName, "base/")
  if (fileName.startswith("BUNDLE-METADATA")):
                 fileName = "META-INF" + remove_prefix(fileName, "BUNDLE-METADATA/")
+if fileName == apkInfo.filename:
+                found = True

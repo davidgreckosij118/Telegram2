ae474bfbfe33395b96780c003f1fe2008634b64a
David <137324959+davidgreckosij118@users.noreply.github.com>
2023-10-06 11:56:11 +0300

    Update apkfrombundle.py

--- apkfrombundle.py
+++ apkfrombundle.py
@@ -39,32 +39,3 @@ def compareApkFromBundle(bundle, apk):
             fileName = bundleInfo.filename
  fileName = remove_prefix(fileName, "base/root/")
             fileName = remove_prefix(fileName, "base/dex/")
-  fileName = remove_prefix(fileName, "base/manifest/")
-            fileName = remove_prefix(fileName, "base/")
- if (fileName.startswith("BUNDLE-METADATA")):
-                fileName = "META-INF" + remove_prefix(fileName, "BUNDLE-METADATA/")
-if fileName == apkInfo.filename:
-                found = True
-  firstFile = apkZip.open(apkInfo, 'r')
-                secondFile = bundleZip.open(bundleInfo, 'r')
- if compareFiles(firstFile, secondFile) != True:
-                    print("APK file %s does not match" % apkInfo.filename)
- return False
-                break
-
-        if found == False:
-            print("file %s not found in APK" % apkInfo.filename)
-  return False
-
-    return True
-
-if __name__ == '__main__':
-    if len(sys.argv) != 3:
-  print("Usage: apkfrombundle <pathToBundle> <pathToApk>")
- sys.exit(1)
-
-
-    if sys.argv[1] == sys.argv[2] or compareApkFromBundle(sys.argv[1], sys.argv[2]) == True:
-   print("APK from bundle!")
-    else:
-  print("APK has difference!")
